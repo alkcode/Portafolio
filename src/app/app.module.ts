4c3a2f23c63e1579960936703cb7733c5dd29a25
alkcode <alek_1604@comunidad.unam.mx>
2022-02-02 09:45:10 -0600

    Portafolio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioComponent } from './pages/inicio/inicio.component';
+import { MenuComponent } from './shared/menu/menu.component';
+import { SkillsComponent } from './pages/skills/skills.component';
+import { ExperienciaComponent } from './pages/experiencia/experiencia.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { ComplementosComponent } from './pages/complementos/complementos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    MenuComponent,
+    SkillsComponent,
+    ExperienciaComponent,
+    FooterComponent,
+    ComplementosComponent
   ],
   imports: [
     BrowserModule,
